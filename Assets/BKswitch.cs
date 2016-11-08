c81976447ae9809e07f7be4c8df4fafab661e744
egaustad033 <erikgaustad@Eriks-MacBook-Pro.local>
2016-11-08 13:15:21 -0800

    Initial Commit with MarbleMadness Game

---
+++ Assets/BKswitch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BKswitch : MonoBehaviour {
+	public GameObject goB;
+	public float xr, yr, zr = 0;
+
+	void start(){
+
+	}
+	void OnMouseDown(){
+	//	goB.transform.rotation = Vector3 (xr, yr, zr);	
+	}
+}
---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+	public bool up = false;
+	public int speed;
+
+	public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetKey("up")){
+				up = true;
+				GetComponent<Rigidbody> ().velocity = transform.up * speed;
+				Instantiate(sphere, new Vector3(60.5f, 28.0f, 45.5f), sphere.transform.rotation);
+				Destroy (sphere.gameObject);
+				sphere = null;
+			}
+			if (Input.GetKey("down")){
+				up = false;
+				GetComponent<Rigidbody> ().velocity = transform.up * -speed;
+				Instantiate(sphere, new Vector3(60.5f, -21.0f, 45.5f), sphere.transform.rotation);
+				Destroy (sphere.gameObject);
+				sphere = null;
+			}if (up == true){
+				up = true;
+				GetComponent<Rigidbody> ().velocity = transform.up * speed;
+				Instantiate(sphere, new Vector3(60.5f, 28.0f, 45.5f), sphere.transform.rotation);
+				Destroy (sphere.gameObject);
+				sphere = null;
+			}
+			if (up == false){
+				up = false;
+				GetComponent<Rigidbody> ().velocity = transform.up * -speed;
+				Instantiate(sphere, new Vector3(60.5f, -21.0f, 45.5f), sphere.transform.rotation);
+				Destroy (sphere.gameObject);
+				sphere = null;
+			}
+	}
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Ball")
+		{
+			sphere = other.gameObject;
+		}
+	}
+}
